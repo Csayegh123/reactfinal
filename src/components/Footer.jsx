3b92ff4dd98ed4c84adaa34cb3e12c9ca96e50bb
Christopher <sayegh.topher@gmail.com>
2021-08-06 13:55:40 -0500

    added code from previous repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <footer class="py-5 bg-dark fixed-bottom">
         <div class="container">
           <p class="m-0 text-center text-white">
-            Copyright &copy; Your Website 2020
+            Copyright &copy; Christopher Sayegh's React Portfolio 2021
           </p>
         </div>
       </footer>
