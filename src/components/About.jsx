1cdec3e777bbbe6b4f13fedc1428f2cc4ab70ce6
Christopher <sayegh.topher@gmail.com>
2021-08-06 13:52:15 -0500

    added code from previous repo

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src=""
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">About</h1>
+            <p>
+             I went to KU for my IT training. I studied many languages and platforms including html, css, javascript, jquery, react, mongodb, Mysql, Nosql, and I learned how to deploy on apps on React, Heroku, and Github.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
